39663a0bdcc2087bab7e33b9d3101f83f38af76f
Junet <bebanstoreserver@gmail.com>
2024-07-04 06:29:01 +0700

    Improved screenshot.js

--- src/commands/utilities/screenshot.js
+++ src/commands/utilities/screenshot.js
@@ -1,12 +1,11 @@
 const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, AttachmentBuilder } = require("discord.js");
 const puppeteer = require("puppeteer")
 const config = require("../../config");
-const { WaitTask } = require("puppeteer");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("screenshot")
-        .setDescription("Take a screenshot of a web")
+        .setDescription("Take a screenshot of a web.")
         .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
         .addStringOption((option) => option
             .setName('url')
@@ -18,42 +17,41 @@ module.exports = {
                 { name: '1366x768', value: '1366x768' },
                 { name: '1920x1080', value: '1920x1080' },)),
 
-    async execute(interaction) {
-    const browser = await puppeteer.launch();
+    async execute(interaction, client) {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+    });
     const page = await browser.newPage();
-    const url = interaction.options.getString('url');
+    let url = interaction.options.getString('url');
+        if (!url.startsWith('http://') && !url.startsWith('https://')) { url = `http://${url}`; }
     const reso = interaction.options.getString('resolution');
-    const load = await interaction.reply({ content: 'Taking screenshot, please wait...', ephemeral: true});
-
-    await page.goto(url, {waitUntil: 'networkidle0'});
-
-
+        
     switch (reso) {
         case '1366x768': {await page.setViewport({width: 1366, height: 768})} break;
         case '1920x1080': {await page.setViewport({width: 1920, height: 1080})} break;
     }
 
-    const layar = await page.screenshot();
-    await browser.close();    
-    const attachment = new AttachmentBuilder()
-        .setFile(layar)
-        .setName('image.png');
-
-    
-    const hasil = new EmbedBuilder()
-        .setAuthor({ name: 'Web Screenshot', iconURL: interaction.guild.iconURL({ dynamic: true }) })
-        .setDescription(`Web: \`\`${url}\`\`
-        Resolution: \`\`${reso}\`\``)
-        .setImage(`attachment://image.png`)
-        .setColor(config.embed.color)
-        .setTimestamp();
         try {
+            interaction.reply({ content: 'Taking screenshot, please wait... The image will be delivered to your DM when done', ephemeral: true });
+            await page.goto(url, {waitUntil: "networkidle2"});
+            const layar = await page.screenshot();
+            const attachment = new AttachmentBuilder()
+        		.setFile(layar)
+        		.setName('image.png');  
+    	    const hasil = new EmbedBuilder()
+        		.setAuthor({ name: 'Web Screenshot', iconURL: 'https://cdn.discordapp.com/icons/1215235509958479894/a_bf57de62bf8946e925e18abd9f761330.gif' })
+        		.setDescription(`Web: \`\`${url}\`\`
+Resolution: \`\`${reso}\`\``)
+        		.setImage(`attachment://image.png`)
+        		.setColor(config.embed.color)
+        		.setTimestamp();
     await interaction.member.send({ content: '', embeds: [hasil], files: [{attachment: layar, name: `image.png` }] });
-    load.edit({ content: 'Image have been sent to your dm.', ephemeral: true });
-        } catch (error) {
-            console.log(error);
-            load.edit({ content: `⚠️Error : \`\`\`Only support http/https format!\`\`\``, ephemeral: true });
-        }
+        } catch (e) {
+            console.log(e);
+            interaction.followUp({ content: `⚠️Error : \`\`Only support http/https protocol\`\``, ephemeral: true });
+        } finally {
+      await browser.close();
+    }
     }
 }
 
