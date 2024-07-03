8f32d0ce1177926a6a842d55c5ff3c93355c82c7
Junet <bebanstoreserver@gmail.com>
2024-07-04 06:32:51 +0700

    Update servericon.js

--- src/commands/utilities/servericon.js
+++ src/commands/utilities/servericon.js
@@ -9,31 +9,14 @@ module.exports = {
 
     async execute(interaction) {
         const guild = interaction.guild
-
         const gif = new ButtonBuilder()
             .setLabel(".gif")
             .setStyle(ButtonStyle.Link)
-            .setURL(guild.iconURL({ dynamic: true, size: 2048, format: "gif" }))
-        
-        const jpeg = new ButtonBuilder()
-            .setLabel(".jpeg")
-            .setStyle(ButtonStyle.Link)
-            .setURL(guild.iconURL({ dynamic: true, size: 2048, format: "jpeg" }))
-        
-        const jpg = new ButtonBuilder()
-            .setLabel(".jpg")
-            .setStyle(ButtonStyle.Link)
-            .setURL(guild.iconURL({ dynamic: true, size: 2048, format: "jpg" }))
-        
+            .setURL(guild.iconURL({ dynamic: true, size: 2048, format: "gif" }))	    
         const png = new ButtonBuilder()
             .setLabel(".png")
             .setStyle(ButtonStyle.Link)
             .setURL(guild.iconURL({ dynamic: true, size: 2048, format: "png" }))
-        
-        const webp = new ButtonBuilder()
-            .setLabel(".webp")
-            .setStyle(ButtonStyle.Link)
-            .setURL(guild.iconURL({ dynamic: true, size: 2048, format: "webp" }))
         const row = new ActionRowBuilder()
             .addComponents(gif, png)
         
