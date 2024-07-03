4e70c368ce8f24840827e20285642606c405e5d2
Junet <bebanstoreserver@gmail.com>
2024-07-04 06:34:58 +0700

    Update send-dm.js

--- src/commands/utilities/send-dm.js
+++ src/commands/utilities/send-dm.js
@@ -4,23 +4,19 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('send-dm')
         .setDescription('Send message to user.')
-        .addUserOption(option => 
-            option.setName('user')
-            .setDescription('User that u want to dm.')
-            .setRequired(true))
-        .addStringOption(option => 
-            option.setName('message')
-            .setDescription('Message/s')
-            .setRequired(true))
-            .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription('User that u want to dm.').setRequired(true))
+        .addStringOption(option => option
+            .setName('message')
+            .setDescription('Message/s').setRequired(true)),
 
-    async execute(interaction) {
-        
+    async execute(interaction) {      
         await interaction.deferReply({ephemeral: true});
         const message = interaction.options.get('message').value;
         const userId = interaction.options.get('user').value;
         target = interaction.guild.members.cache.get(userId)
-
        
         try {
             await target.send(`${message}`)
