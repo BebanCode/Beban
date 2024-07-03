acef4c84cadc05f2787147859773af8265e3eac5
Junet <bebanstoreserver@gmail.com>
2024-07-04 06:36:14 +0700

    Update latency.js

--- src/commands/utilities/latency.js
+++ src/commands/utilities/latency.js
@@ -6,7 +6,7 @@ module.exports = {
     .setName("ping")
     .setDescription("Check the bot's latency")
     .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
-    .setDMPermission(false),
+  
   async execute(interaction) {
     let circles = {
       bagus: "🟢",
@@ -17,11 +17,9 @@ module.exports = {
     await interaction.deferReply();
 
     const pinging = await interaction.editReply({ content: "Pinging..."});
-
     const ws = interaction.client.ws.ping;
     const msgEdit = Date.now() - pinging.createdTimestamp;
 
-    // uptime
     let days = Math.floor(interaction.client.uptime / 86400000);
     let hours = Math.floor(interaction.client.uptime / 3600000) % 24;
     let minutes = Math.floor(interaction.client.uptime / 60000) % 60;
@@ -30,7 +28,6 @@ module.exports = {
     const wsEmoji =
       ws <= 100 ? circles.bagus : ws <= 200 ? circles.okay : circles.jelek;
     const msgEmoji = msgEdit <= 200 ? circles.bagus : circles.jelek;
-
     const pingEmbed = new EmbedBuilder()
       .setColor(config.embed.color)
       .setTimestamp()
