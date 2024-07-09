ae27ead9e3257ba64b1a620164c98012bb69dd6b
Junet <bebanstoreserver@gmail.com>
2024-07-09 19:46:56 +0700

    Create choices.js

---
+++ src/data/choices.js
@@ -0,0 +1,27 @@
+const styles = [
+    { name: "3d-model", value: "3d-model" },
+    { name: "analog-film", value: "analog-film" },
+    { name: "anime", value: "anime" },
+    { name: "cinematic", value: "cinematic" },
+    { name: "comic-book", value: "comic-book" },
+    { name: "digital-art", value: "digital-art" },
+    { name: "enhance", value: "enhance" },
+    { name: "fantasty-art", value: "fantasty-art" },
+    { name: "isometric", value: "isometric" },
+    { name: "line-art", value: "line-art" },
+    { name: "low-poly", value: "low-poly" },
+    { name: "neon-punk", value: "neon-punk" },
+    { name: "origami", value: "origami" },
+    { name: "photographic", value: "photographic" },
+    { name: "pixel-art", value: "pixel-art" },
+    { name: "texture", value: "texture" },
+    { name: "craft-clay", value: "craft-clay" }
+]
+
+const aspectRatio = [
+    { name: "landscape", value: "landscape" },
+    { name: "portrait", value: "portrait" },
+    { name: "square", value: "square" }
+]
+
+module.exports = { styles, aspectRatio }
