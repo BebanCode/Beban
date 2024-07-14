7b5edb96a193592e9556d2f7501fa31abb113dba
Junet <bebanstoreserver@gmail.com>
2024-07-14 21:03:47 +0700

    Update purge.js

--- src/commands/moderation/purge.js
+++ src/commands/moderation/purge.js
@@ -1,30 +1,40 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('purge')
-        .setDescription('Delete the targeted message/s.')
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-        .addStringOption(option => option
-            .setName('amount')
-            .setDescription('The amount of text to delete.').setRequired(true))
-        .addUserOption(option => option
-            .setName('user')
-            .setDescription('The targeted user.').setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setName('purge')
+    .setDescription('Delete the targeted message/s.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addStringOption(option => option
+      .setName('amount')
+      .setDescription('The amount of messages to delete.')
+      .setRequired(true)),
 
-    async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const amount = interaction.options.getString('amount');
+  async execute(interaction) {
+    const amount = interaction.options.getString('amount');
 
-        try {
-            await interaction.channel.messages.fetch({ limit: amount }).then(messages => {
-                const userMessages = messages.filter(m => m.author.id === user.id);
-                interaction.channel.bulkDelete(userMessages);
-                interaction.reply(`${amount} message/s from ${user.tag} has been deleted.`);
-            });
-        } catch (error) {
-            console.error(error);
-            interaction.reply(`⚠️Error :v \`\`${error}\`\``);
-        }
-    },
+    if (!/^\d+$/.test(amount)) {
+      return interaction.reply('Invalid amount. Please enter a positive integer.');
+    }
+
+    const amountInt = parseInt(amount, 10);
+
+    if (amountInt <= 0 || amountInt > 100) {
+      return interaction.reply('Amount must be between 1 and 100.');
+    }
+
+    if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.ManageMessages)) {
+      return interaction.reply('I don\'t have the necessary permissions to manage messages in this channel.');
+    }
+
+    try {
+      await interaction.channel.messages.fetch({ limit: amountInt }).then(messages => {
+        interaction.channel.bulkDelete(messages);
+        interaction.reply(`${amountInt} message/s from ${interaction.channel} has been deleted.`);
+      });
+    } catch (error) {
+      console.error(error);
+      interaction.reply('An error occurred while deleting messages. Please try again later.');
+    }
+  },
 };
