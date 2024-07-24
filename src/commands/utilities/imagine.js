a486061fe7a704d8c36bb846d1a527c9f909cdda
Junet <bebanstoreserver@gmail.com>
2024-07-24 22:02:18 +0700

    Update imagine.js

--- src/commands/utilities/imagine.js
+++ src/commands/utilities/imagine.js
@@ -2,7 +2,8 @@ const { SlashCommandBuilder, PermissionFlagsBits, AttachmentBuilder, EmbedBuilde
 const { Prodia } = require("prodia.js");
 const config = require("../../config")
 const prodia = new Prodia(config.client.prodiakey);
-const { aspectRatio, styles } = require("../../data/choices")
+const { generateImage, wait } = new Prodia(config.client.prodiakey);
+const { aspectRatio, styles, ai_model, samplers } = require("../../data/choices")
 
 
 module.exports = {
@@ -19,7 +20,15 @@ module.exports = {
       .addStringOption(option => option
         .setName("aspect_ratio")
         .setDescription("The aspect ratio of the image.").setRequired(false)
-        .addChoices(aspectRatio)),
+        .addChoices(aspectRatio))
+      .addStringOption(option => option
+        .setName("ai_model")
+        .setDescription("Select the ai model to generate the image.").setRequired(false)
+        .addChoices(ai_model))
+      .addStringOption(option => option
+        .setName("sampler")
+        .setDescription("Select the sampler to generate the image.").setRequired(false)
+        .addChoices(samplers)),
       
   
     async execute(interaction, member) {
@@ -27,10 +36,16 @@ module.exports = {
       const text = interaction.options.getString("prompt");
       const style_preset = interaction.options.getString("style");
       const aspect_ratio = interaction.options.getString("aspect_ratio");
+      const model = interaction.options.getString("ai_model")
+      const sampler = interaction.options.getString("sampler")
+      const cfg_scale = interaction.options.getString("cfg_scale")
+      const steps = interaction.options.getString("steps")
       const result = await prodia.generateImage({
         prompt: text,
-        model: "juggernaut_aftermath.safetensors [5e20c455]",
-        style_preset: style_preset ? style_preset : 'enhance',
+        sampler: sampler ? sampler : 'Euler a',
+        steps: steps ? steps : 20,
+        cfg_scale: cfg_scale ? cfg_scale : 7  ,
+        model: model ? model: 'elldreths-vivid-mix.safetensors [342d9d26]',
         aspect_ratio: aspect_ratio ? aspect_ratio : 'square'
       })
       await interaction.editReply({ content: "Please wait while the image is being generated." })
@@ -41,7 +56,7 @@ module.exports = {
 
         if (job.status === "succeeded") {
             return interaction.editReply(job.imageUrl);
-        }
+        } 
         if (job.status === "failed") {
             return interaction.followUp("Failed to generate image.")
         }
